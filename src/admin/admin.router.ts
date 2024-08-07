ab0167b848dd5b50b9b8ed0047017da06579fb40
Bevence <sitikhubikesh@gmail.com>
2024-08-07 23:45:36 +0545

    fix: prettier fix

--- src/admin/admin.router.ts
+++ src/admin/admin.router.ts
@@ -1,40 +1,41 @@
-import AdminJSExpress from '@adminjs/express';
-import AdminJS from 'adminjs';
-import { Router } from 'express';
-import ConnectPgSimple from 'connect-pg-simple';
-import session from 'express-session';
-import { authenticateUser } from './admin.auth.js';
+import AdminJSExpress from "@adminjs/express"
+import AdminJS from "adminjs"
+import ConnectPgSimple from "connect-pg-simple"
+import { Router } from "express"
+import session from "express-session"
+
+import { authenticateUser } from "./admin.auth.js"
 
 export const expressAuthenticatedRouter = (adminJs: AdminJS, router: Router | null = null) => {
-  const ConnectSession = ConnectPgSimple(session);
+  const ConnectSession = ConnectPgSimple(session)
 
   const sessionStore = new ConnectSession({
     conObject: {
       connectionString: process.env.DATABASE_URL,
-      ssl: process.env.NODE_ENV === 'production',
+      ssl: process.env.NODE_ENV === "production"
     },
-    tableName: 'session',
-    createTableIfMissing: true,
-  });
+    tableName: "session",
+    createTableIfMissing: true
+  })
 
   return AdminJSExpress.buildAuthenticatedRouter(
     adminJs,
     {
       authenticate: authenticateUser,
-      cookieName: process.env.SESSION_NAME ?? 'namuna_adminjs',
-      cookiePassword: process.env.SESSION_SECRET ?? 'namuna_sessionsecret',
+      cookieName: process.env.SESSION_NAME ?? "namuna_adminjs",
+      cookiePassword: process.env.SESSION_SECRET ?? "namuna_sessionsecret"
     },
     router,
     {
       store: sessionStore,
       resave: true,
       saveUninitialized: true,
-      secret: process.env.SESSION_SECRET ?? 'namuna_sessionsecret',
+      secret: process.env.SESSION_SECRET ?? "namuna_sessionsecret",
       cookie: {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: process.env.NODE_ENV === "production"
       },
-      name: process.env.APP_NAME ?? 'Namuna Admin',
+      name: process.env.APP_NAME ?? "Namuna Admin"
     }
-  );
-};
+  )
+}
